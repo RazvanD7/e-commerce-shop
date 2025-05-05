76e17710b468c8809755cfbed0764a05cecfaa6a
noname <147138179+RazvanD7@users.noreply.github.com>
2025-05-05 15:50:03 +0300

    Implemented sorting of products by price

--- backend/Infrastructure/Data/StoreContext.cs
+++ backend/Infrastructure/Data/StoreContext.cs
@@ -17,6 +17,18 @@ namespace Infrastructure.Data
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            if (Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
+            {
+                foreach (var enitityType in modelBuilder.Model.GetEntityTypes())
+                {
+                    var properties = enitityType.ClrType.GetProperties().Where(p => p.PropertyType == typeof(decimal));
+
+                    foreach(var property in properties)
+                    {
+                        modelBuilder.Entity(enitityType.Name).Property(property.Name).HasConversion<double>();                    }
+                }
+            }
         }
 
     }
