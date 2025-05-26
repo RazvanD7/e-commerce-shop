2af2f2794ea913c9e4324c20f6bce943bfc39c0d
noname <147138179+RazvanD7@users.noreply.github.com>
2025-05-27 01:00:41 +0300

    Fixed checkout page bug

--- backend/client/src/app/account/account.service.ts
+++ backend/client/src/app/account/account.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, map, ReplaySubject, tap, catchError, of } from 'rxjs';
+import { BehaviorSubject, map, ReplaySubject, tap, catchError, of, Observable } from 'rxjs';
 import { IUser } from '../shared/models/user';
 import { Router } from '@angular/router';
 
@@ -9,59 +9,60 @@ import { Router } from '@angular/router';
 })
 export class AccountService {
   baseUrl= 'https://localhost:5202/api/';
-  private currentUserSource = new BehaviorSubject<IUser | undefined>(undefined);
+  private currentUserSource = new ReplaySubject<IUser | undefined>(1);
   currentUser$ = this.currentUserSource.asObservable();
-  private rpSource = new ReplaySubject<IUser>(1);
-  rp$ = this.rpSource.asObservable();
 
   constructor(private http: HttpClient, private router: Router) { }
 
-  loadCurrentUser(token: string | null) {
-    if(token === null)
-    {
+  loadCurrentUser(token: string | null): Observable<IUser | undefined> {
+    if (token === null) {
       this.currentUserSource.next(undefined);
       return of(undefined);
     }
+
+    localStorage.setItem('token', token);
+
     let headers = new HttpHeaders();
     headers = headers.set('Authorization', `Bearer ${token}`);
 
     return this.http.get<IUser>(this.baseUrl + 'account', {headers}).pipe(
       map(user => {
         if(user){
-          localStorage.setItem('token', user.token);
           this.currentUserSource.next(user);
+          return user;
         } else {
           this.currentUserSource.next(undefined);
+          return undefined;
         }
-        return user;
       }),
       catchError(error => {
         console.log(error);
-        localStorage.removeItem('token');
         this.currentUserSource.next(undefined);
         return of(undefined);
       })
     );
   }
 
-  login(value: any){
+  login(value: any): Observable<void> {
     return this.http.post<IUser>(this.baseUrl + 'account/login', value).pipe(
       tap(user => {
         if(user){
           localStorage.setItem('token', user.token);
           this.currentUserSource.next(user);
         }
-      })
+      }),
+      map(() => {})
     );
   }
-  register(value: any){
+  register(value: any): Observable<void> {
     return this.http.post<IUser>(this.baseUrl + 'account/register', value).pipe(
       tap(user => {
         if(user){
           localStorage.setItem('token', user.token);
           this.currentUserSource.next(user);
         }
-      })
+      }),
+      map(() => {})
     );
   }
 
--- backend/client/src/app/core/guards/auth.guard.ts
+++ backend/client/src/app/core/guards/auth.guard.ts
@@ -1,25 +1,31 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
-import { map, Observable, tap } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { map, Observable, of, switchMap, take } from 'rxjs';
 import { AccountService } from '../../account/account.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private accountService: AccountService, private router: Router){}
+
+  constructor(private accountService: AccountService, private router: Router) {}
 
   canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Observable<boolean> {
-    return this.accountService.currentUser$.pipe(
-      tap(auth => {
-        if(!auth) {
-          this.router.navigate(['account/login'], {queryParams: {returnUrl: state.url}});
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> {
+    const token = localStorage.getItem('token');
+    return this.accountService.loadCurrentUser(token).pipe(
+      switchMap(() => this.accountService.currentUser$), // Switch to currentUser$ after loadCurrentUser completes
+      map(auth => {
+        if (auth) {
+          return true;
+        } else {
+          this.router.navigate(['/account/login'], {queryParams: {returnUrl: state.url}});
+          return false;
         }
       }),
-      map(auth => !!auth)
+      take(1) // Ensure the guard completes after the first emission post-load
     );
   }
+  
 }
