193eca84c53c9a3c15552c4aaeac6ac174c78a86
noname <147138179+RazvanD7@users.noreply.github.com>
2025-05-26 13:19:53 +0300

    Implemented orders functionality to the backend

--- backend/Infrastructure/Data/GenericRepository.cs
+++ backend/Infrastructure/Data/GenericRepository.cs
@@ -44,5 +44,24 @@ namespace Infrastructure.Data
         {
             return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
         }
+        public async Task<int> CountAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).CountAsync();
+        }
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete(T enitity)
+        {
+            _context.Set<T>().Remove(enitity);
+        }
     }
 }
