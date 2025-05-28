3313ce9d938fa2ad4c5c0e8c7c21078706ab49a4
noname <147138179+RazvanD7@users.noreply.github.com>
2025-05-29 02:16:20 +0300

    Added a contact page

--- backend/client/src/app/core/core.module.ts
+++ backend/client/src/app/core/core.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
-import { TestErrorComponent } from './test-error/test-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
 import {ToastrModule} from 'ngx-toastr';
 import { BreadcrumbComponent, BreadcrumbItemDirective } from 'xng-breadcrumb';
 import { SectionHeaderComponent } from './section-header/section-header.component';
 import { SharedModule } from '../shared/shared.module';
+import { ContactComponent } from './contact/contact.component';
 
 
 
 @NgModule({
-  declarations: [NavBarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent, SectionHeaderComponent],
+  declarations: [NavBarComponent, NotFoundComponent, ServerErrorComponent, SectionHeaderComponent, ContactComponent],
   imports: [
     CommonModule, RouterModule, BreadcrumbComponent, BreadcrumbItemDirective,SharedModule, ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
